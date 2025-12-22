a057f19186d831bf58e20f0e7b427ec7b409c86e
rafee287 <rafee25807@gmail.com>
2025-12-22 11:25:45 +0530

    task1 and task2 added

--- cv_bootcamp/day_1/task_1.py
+++ cv_bootcamp/day_1/task_1.py
@@ -3,3 +3,31 @@ import cv2
 
 #image processing pipeline 
 
+image = cv2.imread("akaza.jpg")
+(h,w,c) = np.shape(image)
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+# gaussian blur 
+blur = cv2.GaussianBlur(gray,(7,7),0)
+
+# Canny edge detection 
+edges = cv2.Canny(blur,threshold1 = 50, threshold2 = 150) 
+
+# binary thresholding
+_,binary_thresh  = cv2.threshold(edges,127,255,cv2.THRESH_BINARY_INV)
+
+# blur_out = blur.reshape(h,w,3)
+# edges_out = edges.reshape(h,w,3)
+# binary_thresh_out = binary_thresh.reshape(h,w,3)
+blur_out = cv2.cvtColor(blur,cv2.COLOR_GRAY2BGR)
+edges_out = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
+binary_thresh_out = cv2.cvtColor(binary_thresh,cv2.COLOR_GRAY2BGR)
+
+h_stack_1 = np.hstack((edges_out,binary_thresh_out))
+h_stack_2 = np.hstack((image,blur_out))
+
+out = np.vstack((h_stack_2,h_stack_1))
+
+cv2.imshow("out",out)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ cv_bootcamp/day_1/task_2.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#loading grayscale image
+image = cv2.imread("akaza.jpg",cv2.IMREAD_GRAYSCALE)
+
+#histogram 
+hist = cv2.calcHist([image], [0], None, [256], [0,256])
+
+# Plot histogram
+plt.plot(hist, color='black')
+plt.title("Grayscale Histogram")
+plt.xlabel("Pixel Intensity")
+plt.ylabel("Frequency")
+plt.savefig("histogram.jpg")
+
+histogram = cv2.imread("histogram.jpg",cv2.IMREAD_GRAYSCALE) 
+# statistics mean and standard deviation
+mean, stdev = cv2.meanStdDev(image)
+median = np.median(image)
+
+print("Mean intensity:", mean[0][0])
+print("Median intensity", median)
+print("Standard deviation:", stdev[0][0])
+
+# Pad img2 to match img1's size
+diff_height = histogram.shape[0] - image.shape[0] 
+#print(diff_height)
+diff_width  = histogram.shape[1] - image.shape[1] 
+#print(diff_width)
+
+# computing even padding
+top    = diff_height // 2
+bottom = diff_height - top
+left   = diff_width // 2
+right  = diff_width - left
+
+image_padded = cv2.copyMakeBorder(
+    image,
+    top , bottom , left, right,
+    cv2.BORDER_CONSTANT, value=(0,0,0)  # black padding
+)
+
+
+out = np.hstack((image_padded,histogram))
+cv2.imshow("out",out)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
